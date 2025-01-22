7e9219546f12a5bdfc87774d6a33d6ff134d0371
mikitro04 <m.tronu4@studenti.unica.it>
2025-01-22 10:24:06 +0100

    test

--- Versione_Processi/main.c
+++ Versione_Processi/main.c
@@ -2,12 +2,11 @@
 
 int main() {
 
-    initscr(); cbreak(); curs_set(0); noecho(); 
+    initscr(); cbreak(); curs_set(0); noecho(); start_color();
 
     WINDOW *punteggio, *gioco, *statistiche, *tane, *spondaSup, *fiume, *spondaInf, *vite, *tempo;
 
     start(punteggio, gioco, statistiche, tane, spondaSup, fiume, spondaInf, vite, tempo);
-
 
     while(1){}
     
